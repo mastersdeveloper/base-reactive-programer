d410a7e34427882cce08f5880572cc00d2ec1c42
David Cruz <dach_092@hotmail.com>
2022-01-04 17:47:54 -0500

    Eliminando todo.

--- api.reactive.webflux.functions.relations.v2/src/main/java/pe/edu/galaxy/training/java/api/reactive/webflux/api/constants/Constants.java
+++
@@ -1,6 +0,0 @@
-package pe.edu.galaxy.training.java.api.reactive.webflux.api.constants;
-
-public class Constants {
-
-	public static final String API_ROUTE_TALLERES="/v1/talleres";
-}
--- api.reactive.webflux.functions.security/src/main/java/pe/edu/galaxy/training/java/api/reactive/webflux/service/gestion/AlumnoService.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.galaxy.training.java.api.reactive.webflux.service.gestion;
-
-import pe.edu.galaxy.training.java.api.reactive.webflux.document.gestion.alumnos.Alumno;
-import pe.edu.galaxy.training.java.api.reactive.webflux.service.generic.GenericService;
-
-public interface AlumnoService extends GenericService<Alumno>{
-	
-}
